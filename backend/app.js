851e12e731b25b5551142db0fd4795a10849bb2e
Audace Kwizera <josueaudace@gmail.com>
2022-05-26 18:35:49 +0200

    fix middleware cors

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,14 @@ const express = require('express');
 // Création de l'application express
 const app = express();
 
-app.use((req, res) => {
-	res.json({ message: 'Votre requête a bien été reçue' })
-})
+// Middleware cors
+// Désactivation sécurité cors pour donner l'accès à tout utilisateur
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 // Exportation de l'application
 module.exports = app;
